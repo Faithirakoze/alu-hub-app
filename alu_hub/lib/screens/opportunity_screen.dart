3277beca7aad7c28329da7301c3fffcc18a4c9dc
k-bridget <k.bridget@alustudent.com>
2026-06-08 13:45:50 +0200

    file structure

---
+++ alu_hub/lib/screens/opportunity_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OpportunityScreen extends StatelessWidget {
+  const OpportunityScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Opportunity')),
+    );
+  }
+}
